c5bf87e45ba7178f2c682a28b352010e0687ecab
Nick <nwe4309@rit.edu>
2022-11-06 00:19:54 -0400

    Created separated pages for each project, added text and images to a few, and added the Reincarnation project

--- js/main.js
+++ js/main.js
@@ -10,17 +10,29 @@ function Hover(mouseIn, textName)
     }
 }
 
-document.querySelector("#TSS").addEventListener("mouseover",function() {Hover(true,"TSS")});
-document.querySelector("#TSS").addEventListener("mouseout",function() {Hover(false,"TSS")});
+document.querySelector("#TSSImg").addEventListener("mouseover",function() {Hover(true,"TSS")});
+document.querySelector("#TSSImg").addEventListener("mouseout",function() {Hover(false,"TSS")});
 
-document.querySelector("#Mispel").addEventListener("mouseover",function() {Hover(true,"Mispel")});
-document.querySelector("#Mispel").addEventListener("mouseout",function() {Hover(false,"Mispel")});
+document.querySelector("#MispelImg").addEventListener("mouseover",function() {Hover(true,"Mispel")});
+document.querySelector("#MispelImg").addEventListener("mouseout",function() {Hover(false,"Mispel")});
 
-document.querySelector("#SeekingSolanum").addEventListener("mouseover",function() {Hover(true,"SeekingSolanum")});
-document.querySelector("#SeekingSolanum").addEventListener("mouseout",function() {Hover(false,"SeekingSolanum")});
+document.querySelector("#SeekingSolanumImg").addEventListener("mouseover",function() {Hover(true,"SeekingSolanum")});
+document.querySelector("#SeekingSolanumImg").addEventListener("mouseout",function() {Hover(false,"SeekingSolanum")});
 
-document.querySelector("#Snowman").addEventListener("mouseover",function() {Hover(true,"Snowman")});
-document.querySelector("#Snowman").addEventListener("mouseout",function() {Hover(false,"Snowman")});
+document.querySelector("#SnowmanImg").addEventListener("mouseover",function() {Hover(true,"Snowman")});
+document.querySelector("#SnowmanImg").addEventListener("mouseout",function() {Hover(false,"Snowman")});
+
+document.querySelector("#SprigganImg").addEventListener("mouseover",function() {Hover(true,"Spriggan")});
+document.querySelector("#SprigganImg").addEventListener("mouseout",function() {Hover(false,"Spriggan")});
+
+document.querySelector("#VanguardImg").addEventListener("mouseover",function() {Hover(true,"Vanguard")});
+document.querySelector("#VanguardImg").addEventListener("mouseout",function() {Hover(false,"Vanguard")});
+
+document.querySelector("#RCTImg").addEventListener("mouseover",function() {Hover(true,"RCT")});
+document.querySelector("#RCTImg").addEventListener("mouseout",function() {Hover(false,"RCT")});
+
+document.querySelector("#SquireImg").addEventListener("mouseover",function() {Hover(true,"Squire")});
+document.querySelector("#SquireImg").addEventListener("mouseout",function() {Hover(false,"Squire")});
 
 function stopAllVideos()
 {
